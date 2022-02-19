a9feb19fe423e675358362ed29abfa42212fb50d
imjord <dean.jay.goat@gmail.com>
2022-02-19 11:59:07 -0700

    student class

---
+++ Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+
+    public Student(int id, String name){
+        this.id = id;
+        this.name = name;
+
+    }
+
+}
