31c8d1e617da3bfd87cca38536a508104584112e
imjord <dean.jay.goat@gmail.com>
2022-02-19 12:38:29 -0700

    teacher class

---
+++ school-system/src/Teacher.java
@@ -0,0 +1,29 @@
+public class Teacher {
+
+    private int id;
+    private String name;
+    private int salary;
+
+
+
+    public Teacher(int id, String name, int salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
+
+}
